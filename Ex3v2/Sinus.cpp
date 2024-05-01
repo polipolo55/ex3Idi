317501337fb327d560b33b7be0e782a5526ca0c5
polipolo55 <pol.casanovas.puig@gmail.com>
2024-05-02 00:19:30 +0200

    cum

--- Ex3v2/Sinus.cpp
+++ Ex3v2/Sinus.cpp
@@ -1,6 +1,10 @@
 #include "Sinus.h"
+#include <cmath>
 
 Sinus::Sinus(QWidget *parent) : QWidget(parent) 
 {
   ui.setupUi(this);
+  for (int i = 0; i < 10; i++) {
+    //double x = i * 2.0 * M_PI / 10;
+  }
 }
