579d60fc480210365a7073d46afeea0cebb42050
polipolo55 <pol.casanovas.puig@gmail.com>
2024-05-06 11:04:00 +0200

    now its the final one

--- Ex3/Cronòmetre.pro
+++
@@ -1,7 +0,0 @@
-TEMPLATE = app
-QT += widgets
-DEPENDPATH += .
-INCLUDEPATH += .    
-FORMS += Cronòmetre.ui
-HEADERS += Cronòmetre.h
-SOURCES += Cronòmetre.cpp main.cpp
--- Ex3/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "Cronòmetre.h"
-
-int main(int argc, char** argv) {
-    QApplication app(argc, argv);
-    Cronometre cronometre;
-    cronometre.show();
-    return app.exec();
-}
